41df40705e9de72b9b2e951904d6b2e4e09feada
Niladitya-Sen <niladityasen.2212@gmail.com>
2023-11-30 23:30:34 +0530

    Update service page to include dynamic dictionary content

--- src/app/[lang]/(services)/services/[slug]/page.tsx
+++ src/app/[lang]/(services)/services/[slug]/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ChatLayout from '@/components/custom/ChatLayout'
+import { getDictionary } from '@/app/[lang]/dictionaries'
 
-export default function Service({ params }: Readonly<{ params: { slug: string } }>) {
+export default async function Service({ params }: Readonly<{ params: { slug: string, lang: string } }>) {
+    const dict = await getDictionary(params.lang);
+    const { bookThisService } = dict.chatPage;
     return (
         <ChatLayout firstMessage={{
-            message: `Do you want to book ${params.slug} service?`,
+            message: bookThisService,
             role: 'captain',
             time: new Date().toLocaleTimeString(
                 'en-US',
